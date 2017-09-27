54a6cafcdb9ca2077cadc4613645d524362d7ed5
MBKeeler <mark.keeler@comcast.net>
2017-09-27 15:31:08 -0400

    Adds functions to events.js
    Adds the module exports and related functions for log-in, log-out and
    sign-up.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -12,6 +12,35 @@ const onSignUp = function (event) {
     .catch(ui.signUpFailure)
 }
 
+// for sign in
+const onSignIn = function (event) {
+  const data = getFormFields(this)
+  event.preventDefault()
+  console.log('sign-in', data)
+  console.log(data)
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+// for signOut
+
+const onSignOut = function (event) {
+  const data = getFormFields(this)
+  event.preventDefault()
+  console.log('onSignOut: ', data)
+  console.log(data)
+  api.signOut(data)
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
+const addHandlers = function () {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#sign-out').on('submit', onSignOut)
+}
+
 module.exports = {
   addHandlers
 }
