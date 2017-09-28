eecc9d26143eb342980386b2a0407e05a29b3780
MBKeeler <mark.keeler@comcast.net>
2017-09-28 13:35:09 -0400

    Adds stubs for additional game logic and UI change
    Adds stub in the game logic for checking if the selected sector is
    occupied.
    
    Adds tweaks to test image by modifying html image to a particular
    height and width.
    
    Other changes are non consequential and were done to show Marie that
    background images set in the CSS are not working

--- assets/scripts/gameLogic.js
+++ assets/scripts/gameLogic.js
@@ -9,6 +9,68 @@ let gameBoard = ['*', '*', '*', '*', '*', '*', '*', '*', '*']
 // check for winner
 // return false for '*' in elements or true for no "*" OR true for a winner
 
+// check if there are empty sectores to claim
+const emptySectors = function (element, index, array) {
+  return element === '*'
+}
+const checkGameContinues = function () {
+  gameBoard.some(emptySectors)
+}
+
+// check to see if sector is occupied
+
+const occupiedSector = function (array, index) {
+  if (array.index === '*') {
+    return false
+  } else if (array.index === 'x' || array.index === 'o') {
+    return true
+  }
+
+// create stub of check winner function.
+const checkWinner = function (array) {
+  console.log('checkWinner() has this array: ', array) // just checks to make sure function is called
+  if (array[0] && array[1] && array[2] === 'x') { // begin row by row check
+    console.log('X is the winner')
+  } else if (array[0] && array[1] && array[2] === 'o') {
+    console.log('O is the winner')
+  } else if (array[3] && array[4] && array[5] === 'x') {
+    console.log('X is the winner')
+  } else if (array[3] && array[4] && array[5] === 'o') {
+    console.log('O is the winner')
+  } else if (array[6] && array[7] && array[8] === 'x') {
+    console.log('X is the winner')
+  } else if (array[6] && array[7] && array[8] === 'o') {
+    console.log('O is the winner')
+  } else if (array[0] && array[3] && array[6] === 'x') { // begin column by column check
+    console.log('X is the winner')
+  } else if (array[0] && array[3] && array[6] === 'o') {
+    console.log('O is the winner')
+  } else if (array[1] && array[4] && array[7] === 'x') {
+    console.log('X is the winner')
+  } else if (array[1] && array[4] && array[7] === 'o') {
+    console.log('O is the winner')
+  } else if (array[2] && array[5] && array[8] === 'x') {
+    console.log('X is the winner')
+  } else if (array[2] && array[5] && array[8] === 'o') {
+    console.log('O is the winner')
+  } else if (array[0] && array[4] && array[8] === 'x') { // begin diagonal check
+    console.log('X is the winner')
+  } else if (array[0] && array[4] && array[8] === 'o') {
+    console.log('O is the winner')
+  } else if (array[2] && array[4] && array[6] === 'x') {
+    console.log('X is the winner')
+  } else if (array[2] && array[4] && array[6] === 'o') {
+    console.log('O is the winner')
+  }
+  return alert('')
+}
+
+
+// game over logic
+// while each element of gameBoard[] has an "*" continue the game,
+// check for winner
+// return false for '*' in elements or true for no "*" OR true for a winner
+
 const emptySectors = function (element, array) {
   return element === '*'
 }
