b27730530dd2658f274dccef9d924c1fe78f6807
MBKeeler <mark.keeler@comcast.net>
2017-09-28 11:02:58 -0400

    Adds fix for Sign In error
    Adds a fix to allow a user to sign up to the development server.
    I was using the develop rather than development in my config
    definition.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -4,7 +4,7 @@ const config = {
   apiOrigins: {
     production: 'https://aqueous-atoll-85096.herokuapp.com/',
     // original  production: 'https://ga-wdi-boston.herokuapp.com'
-    develop: 'http://tic-tac-toe.wdibos.com/'
+    development: 'http://tic-tac-toe.wdibos.com/'
 
   }
 }
