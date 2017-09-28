45b92d3200128049f5d09a11ae65d18ebc71ca55
MBKeeler <mark.keeler@comcast.net>
2017-09-28 13:41:16 -0400

    Add fix to bad paste in gameLogic.js
    gameLogic.js contained a partial duplication of the game logic which
    cause errors.  Fix was to remove the partial duplicated code.

--- assets/scripts/gameLogic.js
+++ assets/scripts/gameLogic.js
@@ -25,6 +25,7 @@ const occupiedSector = function (array, index) {
   } else if (array.index === 'x' || array.index === 'o') {
     return true
   }
+}
 
 // create stub of check winner function.
 const checkWinner = function (array) {
@@ -63,56 +64,4 @@ const checkWinner = function (array) {
     console.log('O is the winner')
   }
   return alert('')
-}
-
-
-// game over logic
-// while each element of gameBoard[] has an "*" continue the game,
-// check for winner
-// return false for '*' in elements or true for no "*" OR true for a winner
-
-const emptySectors = function (element, array) {
-  return element === '*'
-}
-
-const checkGameContinues = function () {
-  gameBoard.some(emptySectors)
-}
-// create stub of check winner function.
-const checkWinner = function (array) {
-  console.log('checkWinner() has this array: ', array) // just checks to make sure function is called
-  if (array[0] && array[1] && array[2] === 'x') { // begin row by row check
-    console.log('X is the winner')
-  } else if (array[0] && array[1] && array[2] === 'o') {
-    console.log('O is the winner')
-  } else if (array[3] && array[4] && array[5] === 'x') {
-    console.log('X is the winner')
-  } else if (array[3] && array[4] && array[5] === 'o') {
-    console.log('O is the winner')
-  } else if (array[6] && array[7] && array[8] === 'x') {
-    console.log('X is the winner')
-  } else if (array[6] && array[7] && array[8] === 'o') {
-    console.log('O is the winner')
-  } else if (array[0] && array[3] && array[6] === 'x') { // begin column by column check
-    console.log('X is the winner')
-  } else if (array[0] && array[3] && array[6] === 'o') {
-    console.log('O is the winner')
-  } else if (array[1] && array[4] && array[7] === 'x') {
-    console.log('X is the winner')
-  } else if (array[1] && array[4] && array[7] === 'o') {
-    console.log('O is the winner')
-  } else if (array[2] && array[5] && array[8] === 'x') {
-    console.log('X is the winner')
-  } else if (array[2] && array[5] && array[8] === 'o') {
-    console.log('O is the winner')
-  } else if (array[0] && array[4] && array[8] === 'x') { // begin diagonal check
-    console.log('X is the winner')
-  } else if (array[0] && array[4] && array[8] === 'o') {
-    console.log('O is the winner')
-  } else if (array[2] && array[4] && array[6] === 'x') {
-    console.log('X is the winner')
-  } else if (array[2] && array[4] && array[6] === 'o') {
-    console.log('O is the winner')
-  }
-  return true
 }
